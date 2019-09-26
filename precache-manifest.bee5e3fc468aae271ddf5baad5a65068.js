55ea9e0fa38f714aa7585daf676721e41dea6bb0
Aditya Mohan <adityamohan@Adityas-MacBook-Air.local>
2019-09-26 13:59:28 +0530

    Updates

---
+++ precache-manifest.bee5e3fc468aae271ddf5baad5a65068.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144be9d71be629c9ece027b4b03f0d09",
+    "url": "/lookr-lite/index.html"
+  },
+  {
+    "revision": "6464ff8b847a4354cba3",
+    "url": "/lookr-lite/static/css/main.13773351.chunk.css"
+  },
+  {
+    "revision": "7530c8ddf27d4884ccf2",
+    "url": "/lookr-lite/static/js/2.20a42577.chunk.js"
+  },
+  {
+    "revision": "6464ff8b847a4354cba3",
+    "url": "/lookr-lite/static/js/main.031a1235.chunk.js"
+  },
+  {
+    "revision": "2396b8dc489c7286d1d4",
+    "url": "/lookr-lite/static/js/runtime~main.68e54035.js"
+  }
+]);
